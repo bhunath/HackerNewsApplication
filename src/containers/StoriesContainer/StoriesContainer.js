f880b45bcf5c6911d6f1a15b247f765ccbd14a26
bhunath <bhupendra191987@gmail.com>
2020-04-12 16:02:42 +0530

    Calling Local Storage through API

--- src/containers/StoriesContainer/StoriesContainer.js
+++ src/containers/StoriesContainer/StoriesContainer.js
@@ -3,6 +3,7 @@ import Story from '../../components/Story/Story'
 import classes from './StoriesContainer.module.css'
 import axios from 'axios'
 import Footer from '../../components/Footer/Footer'
+import {isHideStory,getStoryPoints,hideStory,upvoteStory} from '../../service/HackerNewsApi'
 
 class StoriesContainer extends Component {
 
@@ -14,7 +15,7 @@ class StoriesContainer extends Component {
     };
 
     componentDidMount() {
-        if (this.state.stories.hits.length == 0) {
+        if (this.state.stories.hits.length === 0) {
             axios.get(this.props.url + this.state.pageNumber).then((response => {
                 console.debug(response);
                 this.setState({ stories: response.data });
@@ -29,7 +30,7 @@ class StoriesContainer extends Component {
 
     componentDidUpdate(nextprops, nextstate) {
         console.debug('StoriesContainer.js  |Component Updated ', nextprops, nextstate, this.state.pageNumber);
-        if (nextstate.pageNumber != this.state.pageNumber) {
+        if (nextstate.pageNumber !== this.state.pageNumber) {
             axios.get(this.props.url + this.state.pageNumber).then((response => {
                 console.debug(response);
                 this.setState({ stories: response.data });
@@ -39,14 +40,13 @@ class StoriesContainer extends Component {
 
     upvoteStory = (story) => {
         console.debug('StoryContainer.js | Upvote Story', story);
-
         const storyIndex = this.state.stories.hits.findIndex(s => {
-            return (s.objectID == story.objectID);
+            return (s.objectID === story.objectID);
         })
 
         console.debug('StoryContainer.js | Upvote Story ', storyIndex);
         let clonnedStory = { ...this.state.stories.hits[storyIndex] };
-        console.debug('StoryContainer.js | Upvote Story ', story == clonnedStory)
+        console.debug('StoryContainer.js | Upvote Story ', story === clonnedStory)
         clonnedStory.points += 1;
         let clonnedStories = [...this.state.stories.hits];
         clonnedStories[storyIndex] = clonnedStory;
@@ -56,18 +56,18 @@ class StoriesContainer extends Component {
                     hits: clonnedStories
                 }
             });
-        localStorage.setItem(story.objectID, story.points + 1);
+        upvoteStory(clonnedStory);
     }
 
     hideStory = (story, index) => {
         console.debug('StoryContainer.js | Hide Story', story);
 
         const storyIndex = this.state.stories.hits.findIndex(s => {
-            return (s.objectID == story.objectID);
+            return (s.objectID === story.objectID);
         })
         console.debug('StoryContainer.js | Hide Story ', index, storyIndex);
         let clonnedStory = { ...this.state.stories.hits[storyIndex] };
-        console.debug('StoryContainer.js | Hide Story ', story == clonnedStory)
+        console.debug('StoryContainer.js | Hide Story ', story === clonnedStory)
         clonnedStory.hide = true;
         let clonnedStories = [...this.state.stories.hits];
         clonnedStories[storyIndex] = clonnedStory;
@@ -77,28 +77,16 @@ class StoriesContainer extends Component {
                     hits: clonnedStories
                 }
             });
-        let hidedStories = JSON.parse(localStorage.getItem("HidedStories"));
-        if (hidedStories) {
-            hidedStories.push(story.objectID)
-        } else {
-            hidedStories = [story.objectID]
-        }
-        localStorage.setItem("HidedStories", JSON.stringify(hidedStories));
+        hideStory(clonnedStory);
     }
 
     render() {
         let stories = null;
-        let hidedStories = JSON.parse(localStorage.getItem("HidedStories"));
         if (this.state.stories) {
             stories = this.state.stories.hits.filter(story => {
-                if (true == story.hide) {
-                    return false;
-                } else if (hidedStories && hidedStories.includes(story.objectID)) {
-                    return false;
-                }
-                return true;
+                return !isHideStory(story);
             }).map((story, index) => {
-                const pointsFromStore = localStorage.getItem(story.objectID);
+                let pointsFromStore = getStoryPoints(story);
                 if (pointsFromStore) {
                     story.points = parseInt(pointsFromStore, 10);
                 }
